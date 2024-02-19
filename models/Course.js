3e3a68645102f71d82d545ee4efcd7c8523f8477
samuer216progra <srodriguez-2022165@kinal.edu.gt>
2024-02-18 23:39:22 -0600

    se creo la estructura basica del programa

---
+++ models/Course.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+});
+
+module.exports = mongoose.model('Course', courseSchema);
