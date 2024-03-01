ed49a461f43a45e2347791368003ca1d867a39bb
samuer216progra <srodriguez-2022165@kinal.edu.gt>
2024-03-01 13:15:30 -0600

    se trabajo en la conexion

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Rutas para la autenticación de usuarios
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
